6412c2d980be8414bcbc162a83e0530800b90fd7
Elmar KesslerElmar Kesslergit config --global user.name Elmar <113603505+ElmarKessler@users.noreply.github.com>
2025-10-31 16:37:06 +0100

    add java doc

--- school-system/class-service/src/main/java/com/example/classes/ClassRepository.java
+++ school-system/class-service/src/main/java/com/example/classes/ClassRepository.java
@@ -3,6 +3,9 @@ package com.example.classes;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repository interface for ClassEntity.
+ */
 @Repository
 public interface ClassRepository extends JpaRepository<ClassEntity, Long> {
 }
